47372cc8ef8de788fb49df1175723392806935c3
Iandavidk <eandavid6@gmail.com>
2022-10-23 01:31:14 +0300

    Updated TODOs and defined requires auth function

--- lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
+++ lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
@@ -7,9 +7,9 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = 'dev-0fhyzfbv3ljjvbgs.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+API_AUDIENCE = 'coffeeshopapi'
 
 
 class AuthError(Exception):
@@ -104,18 +104,22 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+    def requires_auth(permission='')
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+            check_permissions(permission, payload)
 
-    return wrapper
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
 
 @app.route('/headers')
 @requires_auth
